d7877b561e9eeb60fcbdfbdbe492e0dcf54732dc
seong9566 <dlgustjd9566@gmail.com>
2022-10-22 17:14:20 +0900

    img.js 생성 후 기업 회원가입 이미지 확인 완료

--- src/main/resources/static/js/companyInform.js
+++ src/main/resources/static/js/companyInform.js
@@ -1,19 +1,3 @@
-//사진 미리 보기
-function setThumbnail(event) {
-    let reader = new FileReader();
-    reader.onload = function (event) {
-        if (document.getElementById("newImg")) {
-            document.getElementById("newImg").remove();
-        }
-        let img = document.createElement("img");
-        let oldImg = $("#oldImg");
-        oldImg.remove();
-        img.setAttribute("src", event.target.result);
-        img.setAttribute("id", "newImg");
-        document.querySelector("#image_container").appendChild(img);
-    };
-    reader.readAsDataURL(event.target.files[0]);
-}
 //주소불러오기
 function findAddr() {
 	new daum.Postcode(
--- src/main/resources/static/js/resumes.js
+++ src/main/resources/static/js/resumes.js
@@ -1,19 +1,4 @@
-//사진 미리 보기
-function setThumbnail(event) {
-    let reader = new FileReader();
-    reader.onload = function (event) {
-        if (document.getElementById("newImg")) {
-            document.getElementById("newImg").remove();
-        }
-        let img = document.createElement("img");
-        let oldImg = $("#oldImg");
-        oldImg.remove();
-        img.setAttribute("src", event.target.result);
-        img.setAttribute("id", "newImg");
-        document.querySelector("#image_container").appendChild(img);
-    };
-    reader.readAsDataURL(event.target.files[0]);
-}
+
 
 // 이력서 작성
 function clickCheck(target) {
