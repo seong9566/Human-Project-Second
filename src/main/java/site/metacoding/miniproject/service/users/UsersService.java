b244aa7c4c87526da6985dc9491378ef57999166
seong9566 <dlgustjd9566@gmail.com>
2022-10-31 01:05:09 +0900

    users복구

--- src/main/java/site/metacoding/miniproject/service/users/UsersService.java
+++ src/main/java/site/metacoding/miniproject/service/users/UsersService.java
@@ -25,96 +25,96 @@ import site.metacoding.miniproject.web.dto.response.etc.SignedDto;
 @RequiredArgsConstructor
 public class UsersService {
 
-	private final UsersDao usersDao;
-	private final CompanyDao companyDao;
-	private final PersonalDao personalDao;
-	private final AlarmDao alarmDao;
-	private final SubscribeDao subscribeDao;
+    private final UsersDao usersDao;
+    private final CompanyDao companyDao;
+    private final PersonalDao personalDao;
+    private final AlarmDao alarmDao;
+    private final SubscribeDao subscribeDao;
 
-	public SignedDto<?> login(LoginDto loginDto) {
-		String loginId = loginDto.getLoginId();
-		String loginPassword = loginDto.getLoginPassword();
-		SignedDto<?> signedDto;
-		Users userinfo = usersDao.findByIdAndPassword(loginId, loginPassword);
-		if (userinfo == null) {
-			return null;
-		}
-		if (userinfo.getPersonalId() != null) {
-			Personal personal = personalDao.findById(userinfo.getPersonalId());
-			signedDto = new SignedDto<>(userinfo.getUsersId(), userinfo.getLoginId(), userinfo.getLoginPassword(),
-					personal.getPersonalId(), null, personal);
-		} else {
-			Company company = companyDao.findById(userinfo.getCompanyId());
-			signedDto = new SignedDto<>(userinfo.getUsersId(), userinfo.getLoginId(), userinfo.getLoginPassword(), null,
-					company.getCompanyId(), company);
-		}
+    public SignedDto<?> login(LoginDto loginDto) {
+        String loginId = loginDto.getLoginId();
+        String loginPassword = loginDto.getLoginPassword();
+        SignedDto<?> signedDto;
+        Users userinfo = usersDao.findByIdAndPassword(loginId, loginPassword);
+        if (userinfo == null) {
+            return null;
+        }
+        if (userinfo.getPersonalId() != null) {
+            Personal personal = personalDao.findById(userinfo.getPersonalId());
+            signedDto = new SignedDto<>(userinfo.getUsersId(), userinfo.getLoginId(), userinfo.getLoginPassword(),
+                    personal.getPersonalId(), null, personal);
+        } else {
+            Company company = companyDao.findById(userinfo.getCompanyId());
+            signedDto = new SignedDto<>(userinfo.getUsersId(), userinfo.getLoginId(), userinfo.getLoginPassword(), null,
+                    company.getCompanyId(), company);
+        }
 
-		return signedDto;
-	}
+        return signedDto;
+    }
 
-	@Transactional(rollbackFor = RuntimeException.class)
-	public void joinPersonal(PersonalJoinDto joinDto) {
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void joinPersonal(PersonalJoinDto joinDto) {
 
-		Personal personal = new Personal(joinDto);
-		personalDao.insert(personal);
+        Personal personal = new Personal(joinDto);
+        personalDao.insert(personal);
 
-		Integer personalId = personal.getPersonalId();
-		joinDto.setPersonalId(personalId);
+        Integer personalId = personal.getPersonalId();
+        joinDto.setPersonalId(personalId);
 
-		Users users = new Users(joinDto);
-		usersDao.insert(users);
+        Users users = new Users(joinDto);
+        usersDao.insert(users);
 
-	}
+    }
 
-	@Transactional(rollbackFor = RuntimeException.class)
-	public void joinCompany(CompanyJoinDto joinDto) {
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void joinCompany(CompanyJoinDto joinDto) {
 
-		Company company = new Company(joinDto);
-		companyDao.insert(company);
+        Company company = new Company(joinDto);
+        companyDao.insert(company);
 
-		Integer companyId = company.getCompanyId();
-		joinDto.setCompanyId(companyId);
+        Integer companyId = company.getCompanyId();
+        joinDto.setCompanyId(companyId);
 
-		Users users = new Users(joinDto);
-		usersDao.insert(users);
+        Users users = new Users(joinDto);
+        usersDao.insert(users);
 
-	}
+    }
 
-	public Integer findUserIdByResumesId(Integer resumesId) {
+    public Integer findUserIdByResumesId(Integer resumesId) {
 
-		Users users = usersDao.findByResumesId(resumesId);
-		return users.getUsersId();
-	}
+        Users users = usersDao.findByResumesId(resumesId);
+        return users.getUsersId();
+    }
 
-	public Integer findUserIdByCompanyId(Integer companyId) {
-		Users users = usersDao.findByCompanyId(companyId);
-		return users.getUsersId();
-	}
+    public Integer findUserIdByCompanyId(Integer companyId) {
+        Users users = usersDao.findByCompanyId(companyId);
+        return users.getUsersId();
+    }
 
-	public Integer checkUserId(String loginId) {
-		Integer checkUser = usersDao.findByLoginId(loginId);
-		return checkUser;
-	}
+    public Integer checkUserId(String loginId) {
+        Integer checkUser = usersDao.findByLoginId(loginId);
+        return checkUser;
+    }
 
-	public List<Alarm> userAlarm(Integer usersId) {
-		List<Alarm> usersAlarm = alarmDao.findByusersId(usersId);
-		return usersAlarm;
-	}
+    public List<Alarm> userAlarm(Integer usersId) {
+        List<Alarm> usersAlarm = alarmDao.findByusersId(usersId);
+        return usersAlarm;
+    }
 
-	public Boolean checkUserAlarm(Integer usersId) {
-		Boolean ischecked = alarmDao.findByUsersIdToAlarmChecked(usersId);
-		return ischecked;
-	}
+    public Boolean checkUserAlarm(Integer usersId) {
+        Boolean ischecked = alarmDao.findByUsersIdToAlarmChecked(usersId);
+        return ischecked;
+    }
 
-	public void userAlarmToCheck(List<Integer> alarmsId) {
-		alarmDao.updateAlarmByIdToCheck(alarmsId);
-	}
+    public void userAlarmToCheck(List<Integer> alarmsId) {
+        alarmDao.updateAlarmByIdToCheck(alarmsId);
+    }
 
-	public void deleteAlarm(Integer alarmId) {
-		alarmDao.deleteById(alarmId);
-	}
+    public void deleteAlarm(Integer alarmId) {
+        alarmDao.deleteById(alarmId);
+    }
 
-	public List<Subscribe> findSubscribeinfoByPersonalId(Integer personalId) {
-		return subscribeDao.findByPersonalId(personalId);
-	}
+    public List<Subscribe> findSubscribeinfoByPersonalId(Integer personalId) {
+        return subscribeDao.findByPersonalId(personalId);
+    }
 }
